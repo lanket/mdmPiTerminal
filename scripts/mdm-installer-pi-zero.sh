60f408c247f835c3e52926447ea3aea55c8239db
devoff <4802297@gmail.com>
2018-04-17 18:46:13 +0300

    3

--- scripts/mdm-installer-pi-zero.sh
+++ scripts/mdm-installer-pi-zero.sh
@@ -42,5 +42,5 @@ pip install -r ~/mdmOrangePiZeroTerminal/Requirements/mdm-orangepizero-pip-requi
 git clone https://github.com/duxingkei33/orangepi_PC_gpio_pyH3
 cd orangepi_PC_gpio_pyH3
 python setup.py install
-
+cd /~
 echo "Testing the installed google assistant. Make a note of the generated "
